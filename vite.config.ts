63033c789af564fc14b1523422f31a404ee7eaaf
Crisak <opensource@1998@gmail.com>
2022-10-14 03:54:24 -0500

    Config import absolutes and environment of tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
 })
